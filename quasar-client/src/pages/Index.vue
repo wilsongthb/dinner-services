f6f3b51ee2aaf0c83b5bdcb3eca927d068b06db9
Wilson Pilco <wilsonaux1@gmail.com>
2020-05-05 22:08:50 -0500

    agregado projecto en quasar

---
+++ quasar-client/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+
+    <h1>hola amigo como estas</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
